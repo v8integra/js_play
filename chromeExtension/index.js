670372828c0af5ec2224a4c3a96c8b95c3c2ed54
v8integra <41065543+v8integra@users.noreply.github.com>
2026-03-08 11:12:02 -0600

    added list items

--- chromeExtension/index.js
+++ chromeExtension/index.js
@@ -1,4 +1,15 @@
-const inputBtn = document.getElementById("input-btn");
-inputBtn.addEventListener("click", function() {
-  console.log("Clicked button");
+let myLeads = ['greatLeads', 'awesomeLeads', 'goodLeads'];
+let listItem = [];
+
+const input = document.getElementById('input');
+const leadsUl = document.getElementById('leads-ul');
+
+const inputBtn = document.getElementById('input-btn');
+inputBtn.addEventListener('click', function() {
+  //myLeads.push();
+  for(let i = 0; i < myLeads.length; i++){
+    leadsUl.innerHTML += `<li> ${myLeads[i]} </li>`
+  }
 });
+
+

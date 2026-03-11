1f2e59fa3e3ac45f471c18b4f30268fad2eb57fa
unknown <keith.francis83@gmail.com>
2026-03-11 12:00:00 -0600

    set list of leads as links

--- chromeExtension/index.js
+++ chromeExtension/index.js
@@ -12,7 +12,7 @@ inputBtn.addEventListener("click", function() {
 function renderLeads() {
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
-        listItems += "<li>" + myLeads[i] + "</li>"
+        listItems += `<li> <a href='${myLeads[i]}' target="_blank"> ${myLeads[i]} </a> </li>`
     }
     leadsUl.innerHTML = listItems  
 }

b81dd995eec56102ad4ed65730d7bc8606d65a28
unknown <keith.francis83@gmail.com>
2026-03-12 15:55:55 -0600

    added manifest, added delete option, setup localstorage

--- chromeExtension/index.js
+++ chromeExtension/index.js
@@ -1,18 +1,34 @@
 let myLeads = []
+let myLeadsStore = ''
 const input = document.getElementById("input")
 const inputBtn = document.getElementById("input-btn")
+const clearBtn = document.getElementById("clear-btn")
 const leadsUl = document.getElementById("leads-ul")
 
-inputBtn.addEventListener("click", function() {
+if(localStorage.getItem('myLeads')) {
+  myLeads = JSON.parse(localStorage.getItem('myLeads'));
+  renderLeads();
+}
+
+inputBtn.addEventListener('click', function() {
     myLeads.push(input.value)
+    myLeadsStore = JSON.stringify(myLeads);
+    localStorage.setItem('myLeads', myLeadsStore);
     input.value = '';
     renderLeads()
+});
+
+clearBtn.addEventListener('click', function() {
+  localStorage.removeItem('myLeads');
+  myLeads = [];
+  renderLeads();
 })
 
 function renderLeads() {
-    let listItems = ""
+    let listItems = "";
+
     for (let i = 0; i < myLeads.length; i++) {
-        listItems += `<li> <a href='${myLeads[i]}' target="_blank"> ${myLeads[i]} </a> </li>`
+        listItems += `<li> <a href='${myLeads[i]}' target='_blank'> ${myLeads[i]} </a> </li>`
     }
     leadsUl.innerHTML = listItems  
 }

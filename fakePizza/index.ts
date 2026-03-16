9febb7c13804842d7e5269a881bc5f1db3c04f00
unknown <keith.francis83@gmail.com>
2026-03-16 11:35:53 -0600

    added omit to pizzaObj

--- fakePizza/index.ts
+++ fakePizza/index.ts
@@ -10,19 +10,28 @@ type Order = {
     status: "ordered" | "completed"
 }
 
-const menu: Pizza[] = [
-    { id: 1, name: "Margherita", price: 8 },
-    { id: 2, name: "Pepperoni", price: 10 },
-    { id: 3, name: "Hawaiian", price: 10 },
-    { id: 4, name: "Veggie", price: 9 },
-]
-
 let cashInRegister: number = 100
 let nextOrderId: number = 1
+let nextPizzaId: number = 0
 const orderQueue: Order[] = []
 
-function addNewPizza(pizzaObj: Pizza): void {
-    menu.push(pizzaObj)
+const menu: Pizza[] = [
+    { id: nextPizzaId++, name: "Margherita", price: 8 },
+    { id: nextPizzaId++, name: "Pepperoni", price: 10 },
+    { id: nextPizzaId++, name: "Hawaiian", price: 10 },
+    { id: nextPizzaId++, name: "Veggie", price: 9 },
+]
+
+
+
+function addNewPizza(pizzaObj: Omit<Pizza, "id">): Pizza {
+    const newPizzaObj: Pizza = {
+        id: nextPizzaId++,
+        ...pizzaObj
+    }
+    
+    menu.push(newPizzaObj)
+    return newPizzaObj
 }
 
 function placeOrder(pizzaName: string): Order | undefined {
@@ -55,9 +64,9 @@ function getPizzaDetail(identifier: string | number): Pizza | undefined {
     }
 }
 
-addNewPizza({ id: 5, name: "Chicken Bacon Ranch", price: 12 })
-addNewPizza({ id: 6, name: "BBQ Chicken", price: 12 })
-addNewPizza({ id: 7, name: "Spicy Sausage", price: 11 })
+addNewPizza({ name: "Chicken Bacon Ranch", price: 12 })
+addNewPizza({ name: "BBQ Chicken", price: 12 })
+addNewPizza({ name: "Spicy Sausage", price: 11 })
 
 placeOrder("Chicken Bacon Ranch")
 completeOrder(1)
